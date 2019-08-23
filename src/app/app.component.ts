8679a981c23a049e06b85f73fb4a56cab3d3852a
Guillermo Solano <ing.guillermosolano@gmail.com>
2019-08-23 15:30:23 -0500

    Inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NzMessageService } from "ng-zorro-antd";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'ant-gllrm';
+  title = "ant-gllrm";
+  cancel(): void {
+    this.nzMessageService.info("click cancel");
+  }
+
+  confirm(): void {
+    this.nzMessageService.info("click confirm");
+  }
+  constructor(private nzMessageService: NzMessageService) {}
 }
